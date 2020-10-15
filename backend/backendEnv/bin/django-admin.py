8d9b4bb41d8ec6111804c3598b88b28e3e8d6942
Keisha Mukasa <keisha.mukasa@tufts.edu>
2020-10-15 14:04:25 -0400

    changed folder structure of the backend

---
+++ backend/backendEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/keishamukasa/Documents/Tufts/Fall_2020/JumboCode/OCCUR/backend/backendEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
