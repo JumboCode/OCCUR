72c8e6591d4462a14854bf0a626e7284c1a1869f
Luke Taylor <luke@deentaylor.com>
2020-11-05 17:57:01 -0500

    Add Auth0Provider around app

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-export default function AppContainer({ Component }) {
+export default function AppContainer({ Component, pageProps }) {
   return (
-    <div>
-      Secret message
-      <Component />
-    </div>
+    <Auth0Provider
+      domain="occur.us.auth0.com"
+      clientId="OUr5pR1GCGKp7krFCbcZ1SwkxZLwTYo8"
+      redirectUri={process.env.NEXT_PUBLIC_BASE_URL}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
   );
 }
 
 AppContainer.propTypes = {
-  Component: PropTypes.node.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
 };
