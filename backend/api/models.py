de8b7f89f526137af4e4b7a3d0aa47d09c9e626d
Keisha Mukasa <keisha.mukasa@tufts.edu>
2020-11-05 13:03:24 -0500

    added locations model data

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Resource(models.Model):
+    # Pass
+    name = models.CharField(max_length=30),
+    location = ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class Location(models.Model):
+    street_address = models.CharField(max_length=30),
+    city = models.CharField(max_length=15),
+    state = models.CharField(max_length=30),
+    zip_code = models.CharField(max_length=5), 
+    latitude = models.FloatField(max_length=30),
+    longitude = models.FloatField(max_length=30),
+
+    
