f68c1549111f72c7e122728e73b1541e23e81402
Luke Taylor <luke@deentaylor.com>
2020-11-05 17:57:22 -0500

    Add login button on homepage

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Home() {
+  const auth = useAuth0();
+
   return (
     <div>
       Hello, world!
+      <button type="button" onClick={() => auth.loginWithRedirect()}>
+        Click me
+      </button>
     </div>
   );
 }
