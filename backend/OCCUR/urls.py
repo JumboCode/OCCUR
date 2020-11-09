1487aaefddf0de1dfe8596329d7be09d12e0e60c
tinaking28 <tina.king528@gmail.com>
2020-11-08 20:38:01 -0500

    created the dummy api and tested with curl

--- backend/OCCUR/urls.py
+++ backend/OCCUR/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/new/', views.DummyAPI.as_view()),
+    path('admin/', admin.site.urls
+    ),
 ]
