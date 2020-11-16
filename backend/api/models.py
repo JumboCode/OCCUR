d8384c6d695a9d9c4f98ea6f0daa1967bcc016a0
de08oei <deannaoei123@gmail.com>
2020-11-15 19:58:23 -0500

    updated models required fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Models
 class Location(models.Model):
-    street_address = models.CharField(max_length=30, blank=True)
-    city = models.CharField(max_length=15, blank=True)
-    state = models.CharField(max_length=30, blank=True)
-    zip_code = models.CharField(max_length=5, blank=True) 
-    latitude = models.FloatField(max_length=30, null=True)
-    longitude = models.FloatField(max_length=30, null=True)
+    street_address  = models.CharField(max_length=30, blank=True)
+    city            = models.CharField(max_length=15, blank=True)
+    state           = models.CharField(max_length=30, blank=True)
+    zip_code        = models.CharField(max_length=5, blank=True) 
+    latitude        = models.FloatField(max_length=30, null=True, blank=True)
+    longitude       = models.FloatField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return "{},\n{},\n{}, {}".format(
@@ -29,17 +29,17 @@ class Resource(models.Model):
     ('OTHER', 'Other'),
 ]
 
-    name = models.CharField(max_length=30, blank=True)
-    organization = models.CharField(max_length=30, blank=True)
-    category = models.CharField(choices=RESOURCE_CATEGORIES, default='OTHER', max_length=20)
-    startDate = models.DateField(auto_now=False, auto_now_add=False, null=True)
-    endDate = models.DateField(auto_now=False, auto_now_add=False, null=True)
-    time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
-    flyer = models.URLField(null=True)
-    link = models.URLField(null=True)
-    location = models.ForeignKey('Location', on_delete=models.CASCADE)
-    zoom = models.URLField(null=True)
-    description = models.TextField(blank=True)
+    name            = models.CharField(max_length=30, blank=False)
+    organization    = models.CharField(max_length=30, blank=False)
+    category        = models.CharField(choices=RESOURCE_CATEGORIES, default='OTHER', max_length=20, null=True, blank=False)
+    startDate       = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    endDate         = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    time            = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    flyer           = models.URLField(null=True, blank=True)
+    link            = models.URLField(null=True, blank=True)
+    location        = models.ForeignKey('Location', on_delete=models.CASCADE, blank=True)
+    zoom            = models.URLField(null=True, blank=True)
+    description     = models.TextField(blank=False)
     
     def __str__(self):
         return "Resource: {},\n Provided by: {}\n".format(
