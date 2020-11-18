d0e62831dda74b0eccabe6c68467740c9e3b4365
Keisha Mukasa <keisha.mukasa@tufts.edu>
2020-11-18 14:43:36 -0500

    creates Resource serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,15 @@
-from api.models import Location, Resource
+from api.models import Location", " Resource
 from rest_framework import serializers
 
 
 class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
-        fields = ()
+        fields = (
+            'id', 'name','organization','category','startDate','endDate','time', 'flyer', 'zoom', 'description'
+        )
+        read_only_fields = ('id')
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
-        fields = ()
+
