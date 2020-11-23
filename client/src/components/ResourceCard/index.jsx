8e269ef699f835ac2ea6ac323bf2d44f33383e33
Brendon <bbellevue20@gmail.com>
2020-11-22 20:33:50 -0500

    some changes

--- client/src/components/ResourceCard/index.jsx
+++ client/src/components/ResourceCard/index.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ResourceCard.module.scss';
 
+import ClockIcon from '../../../public/clock.svg';
+
 export default function ResourceCard({
   resourceTitle, organization, startDate, endDate, location, imageSrc, startTime, endTime
 }) {
   return (
     <div className={styles.container}>
       <div className={styles.ResourceInfo}>
-        <div style={{ 'background-color': getRandomColor() }} className={styles.leftside}>
+        <div className={styles.leftside}>
           {/* <h1>hi</h1> */}
         </div>
 
         <div className={styles.rightside}>
           <h3>{resourceTitle}</h3>
           <h6>{organization}</h6>
+          <ClockIcon />
           <h4>{startDate.toLocaleDateString()}</h4>
           <h4>{location}</h4>
           <h4>{startTime.toLocaleTimeString('en-US')}</h4>
@@ -24,22 +27,6 @@ export default function ResourceCard({
     </div>
   );
 }
-
-function getRandomNumber(low, high) {
-  const r = Math.floor(Math.random() * (high - low + 1)) + low;
-  return r;
-}
-function getRandomColor() {
-  const characters = '0123456789ABCDEF';
-  let color = '#';
-
-  for (let i = 0; i < 6; i++) {
-    color += characters[getRandomNumber(0, 15)];
-  }
-
-  return color;
-}
-
 
 ResourceCard.propTypes = {
   resourceTitle: PropTypes.string.isRequired,
