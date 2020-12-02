fd6e2da5ad30e29a41bd3fb9846410ad6733cb0c
tinaking28 <tina.king528@gmail.com>
2020-12-01 20:29:17 -0500

    added delete, listing get, and create for location

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView
 from rest_framework.response import Response
 from api.serializers import ResourceSerializer
+from api.serializers import LocationSerializer
 from api.models import Resource
 from api.models import Location
 
 class ResourceCreate(CreateAPIView):
     serializer_class = ResourceSerializer
     
-    def create(self, request, format=None):
+    def create(self, request, *args, **kwargs):
         # validate resource
         # Checks image and processes image_data
             # If image is url store URL 
@@ -21,3 +22,44 @@ class ResourceCreate(CreateAPIView):
         # Returns resource object
 
         return Response({"message": "Hello, world!"})
+
+class LocationCreate(CreateAPIView):
+    serializer_class = LocationSerializer
+    
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+class ResourceDestroy(DestroyAPIView):
+    queryset = Resource.objects.all()
+    lookup_field = 'id'
+
+    def delete(self, request, *args, **kwargs):
+        Resource_id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('resource_data_{}'.format(Resource_id))
+        return response
+
+
+
+class LocationDestroy(DestroyAPIView):
+    queryset = Location.objects.all()
+    lookup_field = 'id'
+
+    def delete(self, request, *args, **kwargs):
+        Location_id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('location_data_{}'.format(Location_id))
+        return response
+
+
+class ResourceList(ListAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+class LocationList(ListAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
