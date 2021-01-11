2ec2531263590e843fc712034e88c4fb35ae28bd
Luke Taylor <luke@deentaylor.com>
2021-01-11 17:10:26 -0500

    Allow backend to run on Heroku host

--- backend/OCCUR/settings.py
+++ backend/OCCUR/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'occur-backend.herokuapp.com'
+]
 
 
 # Application definition
