2cd615d3965611071cd601e98602fa68a04c7134
Jackson Parsells <john.parsells@mumvia.com>
2021-02-25 13:20:38 -0500

    added logo and coloring and fixed paths

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,24 +4,47 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <div className={styles.base}>
-      <div>another child</div>
-
-      <div className={styles.address}>
-        OCCUR Business Office
+      <div>
+        Join Our Email List
         <br />
-        360 14th Street
         <br />
-        Ste 100
-        <br />
-        Oakland, CA 94612
-        <br />
-        US
-        <br />
-        (510) 839 – 2440
-        <br />
+        Donate Today
       </div>
 
-      <div>third child</div>
+      <div>
+        OCCUR Business Office
+        <br />
+        <br />
+        <div className={styles.address}>
+          360 14th Street
+          <br />
+          Ste 100
+          <br />
+          Oakland, CA 94612
+          <br />
+          US
+        </div>
+        <div className={styles.phoneNumber}>
+          (510) 839 – 2440
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className={styles.logo}>
+          <img className={styles.logo} src="/images/OCCUR_logo.png" alt="OCCUR" />
+        </div>
+      </div>
+
+      <div>
+        Contact Us
+        <div className={styles.icons}>
+          <img className={styles.icon} src="/images/facebook_icon.png" alt="Facebook" />
+          <img className={styles.icon} src="/images/twitter_icon.png" alt="Twitter" />
+          <img className={styles.icon} src="/images/linkedin_icon.png" alt="Linkedin" />
+          <img className={styles.icon} src="/images/instagram_icon.png" alt="Instagram" />
+        </div>
+      </div>
     </div>
   );
 }
