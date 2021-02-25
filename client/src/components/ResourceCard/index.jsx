1acab852196d25ce6604e5ccaec1603934f8d28f
Luke Taylor <luke@deentaylor.com>
2021-02-25 13:46:46 -0500

    Begin share button component

--- client/src/components/ResourceCard/index.jsx
+++ client/src/components/ResourceCard/index.jsx
@@ -32,8 +32,6 @@ export default function ResourceCard({
             {location}
           </p>
         </div>
-        
-        
       </div>
     </div>
   );
---
+++ client/src/components/ShareButton/ShareButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ShareButton() {
+  return (
+    <button type="button">Share</button>
+  );
+}
