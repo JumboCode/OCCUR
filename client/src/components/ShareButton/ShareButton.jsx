3dbcb6f7c341f80e66d14a75b2295493869f706a
Luke Taylor <luke@deentaylor.com>
2021-03-07 19:10:48 -0500

    Functionality for ShareButton
    also remove Instagram
    
    Co-authored-by: JacksonParsells <JacksonParsells@users.noreply.github.com>

--- client/src/components/ShareButton/ShareButton.jsx
+++ client/src/components/ShareButton/ShareButton.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styles from './ShareButton.module.scss';
 
 import FacebookIcon from '../../../public/icons/logo-facebook.svg';
 import TwitterIcon from '../../../public/icons/logo-twitter.svg';
-import InstagramIcon from '../../../public/icons/logo-instagram.svg';
 import LinkedinIcon from '../../../public/icons/logo-linkedin.svg';
 
-export default function ShareButton() {
+export default function ShareButton({ url }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const openWindow = () => {
     setIsOpen(!isOpen);
   };
 
+  const encodedURL = encodeURIComponent(url);
   return (
     <div className={styles.base}>
       <button type="button" onClick={openWindow}>
@@ -23,10 +24,30 @@ export default function ShareButton() {
         isOpen
           ? (
             <div className={styles.shareIcons}>
-              <span className={styles.facebook}><FacebookIcon /></span>
-              <span className={styles.twitter}><TwitterIcon /></span>
-              <span className={styles.instagram}><InstagramIcon /></span>
-              <span className={styles.linkedin}><LinkedinIcon /></span>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.facebook.com/dialog/feed?app_id=447289576469469&display=page&link=${encodedURL}`}
+                className={styles.facebook}
+              >
+                <FacebookIcon />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://twitter.com/intent/tweet?url=${encodedURL}`}
+                className={styles.twitter}
+              >
+                <TwitterIcon />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodedURL}`}
+                className={styles.linkedin}
+              >
+                <LinkedinIcon />
+              </a>
             </div>
           )
           : null
@@ -34,3 +55,6 @@ export default function ShareButton() {
     </div>
   );
 }
+ShareButton.propTypes = {
+  url: PropTypes.string.isRequired,
+};
