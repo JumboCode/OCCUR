cceb2ba8bc34022dc161606b4bdd0dcd5d3a9869
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-07 19:30:23 -0500

    added function skeleton for admin endpoints

--- backend/api/admin_views.py
+++ backend/api/admin_views.py
@@ -1,5 +1,10 @@
-from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
 
-@api_view(['GET'])
-def get_admins():
-    
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
+# special permissions needed because GET is usually readonly (and therefore visible to all),
+# but we don't want just anyone seeing all of our admins' info
+@permission_classes([IsAuthenticated])
+def get_admins(request):
+    # now we would perform different actions based on request method
+    return JsonResponse({'message': 'Hello World!'})
