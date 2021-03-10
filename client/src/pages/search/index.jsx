9ad00f10a7ff7a5ea979c46b5fe49f7028d3faa9
Luke Taylor <luke@deentaylor.com>
2021-03-09 19:29:55 -0500

    Remove old demo filtering functionality from search page

--- client/src/pages/search/index.jsx
+++ client/src/pages/search/index.jsx
@@ -1,24 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ResourceCard from 'components/ResourceCard';
 
-const searchResults = [
-  { name: 'My first result', description: 'hi' },
-  { name: 'My second result', description: 'this is the second result' },
-];
-
 export default function SearchPage() {
-  const [query, setQuery] = useState('');
-
   return (
     <div>
       <h1>Search</h1>
-      <input
-        type="text"
-        placeholder="Enter your search here"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-
       <ResourceCard
         resourceTitle="Oakland Food Drive"
         imageSrc="https://source.unsplash.com/random/600x800?dummy=1"
@@ -41,19 +27,6 @@ export default function SearchPage() {
         startTime={new Date('2021-03-17T12:00:00Z')}
         endTime={new Date('2021-03-17T21:00:00Z')}
       />
-
-      <div>
-        {
-          searchResults
-            .filter((s) => s.name.includes(query))
-            .map((s) => (
-              <div key={s.name}>
-                <h3>{s.name}</h3>
-                <p>{s.description}</p>
-              </div>
-            ))
-        }
-      </div>
     </div>
   );
 }
