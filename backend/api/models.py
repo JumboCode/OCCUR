d338b675d092e449320c1e85b2b8e8ef00daa6d0
keisha mukasa <keisha.mukasa@gmail.com>
2021-03-19 15:38:05 -0400

    modified models to include recurrence information

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,18 +12,31 @@ class Resource(models.Model):
     ('EVENTS', 'Events'),
     ('WIFI', 'Free Wifi'),
     ('OTHER', 'Other'),
-]
+    ]
+
+    DAYS_OF_WEEK = [
+        ('SUN', 'Sunday'),
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday')
+    ]
 
     name            = models.CharField(max_length=30, blank=False)
     organization    = models.CharField(max_length=30, blank=False)
     category        = models.CharField(choices=RESOURCE_CATEGORIES, default='OTHER', max_length=20, null=True, blank=False)
     startDate       = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     endDate         = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
-    time            = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    startTime       = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    endTime         = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    isRecurring     = modules.BooleanField(null=True, blank=True)
+    recurrenceDays  = modules.MultipleChoiceField(choices=DAYS_OF_WEEK,null=True, blank=False, default=[])
     flyer           = models.URLField(null=True, blank=True)
-    flyer_id        = models.CharField(max_length=30, null=True, blank=True)
+    flyerId         = models.CharField(max_length=30, null=True, blank=True)
     link            = models.URLField(null=True, blank=True)
-    zoom            = models.URLField(null=True, blank=True)
+    meetingLink     = models.URLField(null=True, blank=True)
     description     = models.TextField(blank=False)
     
     def __str__(self):
