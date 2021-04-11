504d045fd92bd57bf276d8767cb4d22939de3235
Luke Taylor <luke@deentaylor.com>
2021-04-11 19:47:13 -0400

    Begin second page of admin login

--- client/src/components/AdminLogin/AdminLogin.jsx
+++ client/src/components/AdminLogin/AdminLogin.jsx
@@ -10,11 +10,13 @@ const cx = classNames.bind(styles);
 export default function AdminLogin() {
   const [email, setEmail] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
+  const [sentEmail, setSentEmail] = useState(false);
 
   const login = useCallback(() => {
     doLogin(email)
       .then(() => {
         setErrorMessage(null);
+        setSentEmail(true);
       })
       .catch((err) => {
         if (['bad.email', 'bad.connection'].includes(err?.code)) setErrorMessage('Please enter a valid email');
@@ -27,14 +29,37 @@ export default function AdminLogin() {
 
   return (
     <div className={cx('base')}>
-      <h1>Admin Login</h1>
-      <div className={cx('error', { hidden: !errorMessage })}>
-        {errorMessage}
-      </div>
-      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
-      <button type="button" onClick={login}>
-        Log in
-      </button>
+      {
+        !sentEmail
+          ? (
+            <>
+              <h1>Admin Login</h1>
+              <div className={cx('error', { hidden: !errorMessage })}>
+                {errorMessage}
+              </div>
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
+              <button type="button" className={cx('login-button')} onClick={login}>
+                Log in
+              </button>
+            </>
+          )
+          : (
+            <>
+              <h1>Check your email!</h1>
+              <div className={cx('message')}>
+                Email confirmation sent to
+                <br />
+                <i>{email}</i>
+              </div>
+              <div className={cx('message', 'small')}>
+                Didn’t receive an email?
+                <button type="button" className={cx('reset-button')} onClick={() => setSentEmail(false)}>
+                  Click to resend
+                </button>
+              </div>
+            </>
+          )
+      }
     </div>
   );
 }
