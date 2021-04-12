8eb9f73faef08b9e376c03ac9486aa02ae1d2c34
Luke Taylor <luke@deentaylor.com>
2021-04-12 14:35:11 -0400

    Add “logged in” state to AdminLogin

--- client/src/components/AdminLogin/AdminLogin.jsx
+++ client/src/components/AdminLogin/AdminLogin.jsx
@@ -7,6 +7,7 @@ import EmailIcon from '../../../public/icons/email.svg';
 
 import styles from './AdminLogin.module.scss';
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/router';
 
 const cx = classNames.bind(styles);
 
@@ -15,10 +16,11 @@ export default function AdminLogin() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [sentEmail, setSentEmail] = useState(false);
 
-  const { ready: authReady, login: doLogin } = useAuth();
+  const { isAuthenticated, ready: authReady, login, logout, identity } = useAuth();
+  const router = useRouter();
 
-  const login = useCallback(() => {
-    doLogin(email)
+  const doLogin = useCallback(() => {
+    login(email)
       .then(() => {
         setErrorMessage(null);
         setSentEmail(true);
@@ -32,45 +34,65 @@ export default function AdminLogin() {
       });
   }, [email]);
 
+  const doLogout = useCallback(() => {
+    logout();
+    router.push('/');
+  });
+
+  // Login page 1: enter email address and send email
+  const page1 = (
+    <>
+      <h1>Admin Login</h1>
+      <div className={cx('error', { hidden: !errorMessage })}>
+        {errorMessage}
+      </div>
+      <div className={cx('input-group')}>
+        <EmailIcon />
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
+      </div>
+      <button type="button" className={cx('login-button')} onClick={doLogin} disabled={!authReady}>
+        Log in
+      </button>
+    </>
+  );
+
+  // Login page 2: Instructions for once you're done with that
+  const page2 = (
+    <>
+      <h1 style={{ marginBottom: '0.5em' }}>Check your email!</h1>
+      <EmailIllustration className={cx('illustration')} />
+      <div className={cx('message')} style={{ marginTop: '1em' }}>
+        Email confirmation sent to
+        <br />
+        <i>{email}</i>
+      </div>
+      <div className={cx('message', 'small')} style={{ marginTop: '2em' }}>
+        Didn’t receive an email?
+        <button type="button" className={cx('reset-button')} onClick={() => setSentEmail(false)}>
+          Click to resend
+        </button>
+      </div>
+    </>
+  );
+
+  const loginInterface = (!sentEmail ? page1 : page2);
+  const logoutInterface = (
+    <>
+      <h1 style={{ marginBottom: 0 }}>Welcome</h1>
+      <p className={cx('message')} style={{ marginBottom: 0 }}>
+        You’re logged in as
+        {' '}
+        {identity?.name}
+      </p>
+      <button type="button" className={cx('login-button')} onClick={doLogout} disabled={!authReady}>
+        Log out
+      </button>
+    </>
+  );
+
   return (
     <div className={cx('base')}>
-      {
-        !sentEmail
-          // First page: enter email address and send email
-          ? (
-            <>
-              <h1>Admin Login</h1>
-              <div className={cx('error', { hidden: !errorMessage })}>
-                {errorMessage}
-              </div>
-              <div className={cx('input-group')}>
-                <EmailIcon />
-                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
-              </div>
-              <button type="button" className={cx('login-button')} onClick={login} disabled={!authReady}>
-                Log in
-              </button>
-            </>
-          )
-          // Instructions for once you're done with that
-          : (
-            <>
-              <h1 style={{ marginBottom: '0.5em' }}>Check your email!</h1>
-              <EmailIllustration className={cx('illustration')} />
-              <div className={cx('message')} style={{ marginTop: '1em' }}>
-                Email confirmation sent to
-                <br />
-                <i>{email}</i>
-              </div>
-              <div className={cx('message', 'small')} style={{ marginTop: '2em' }}>
-                Didn’t receive an email?
-                <button type="button" className={cx('reset-button')} onClick={() => setSentEmail(false)}>
-                  Click to resend
-                </button>
-              </div>
-            </>
-          )
-      }
+      {!isAuthenticated ? loginInterface : logoutInterface}
     </div>
   );
 }
