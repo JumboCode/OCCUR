304e3c682672f8d2ad21a7b2038fde014f229c64
Luke Taylor <luke@deentaylor.com>
2021-04-18 17:52:08 -0400

    Add support for PATCH requests

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,7 @@ import { useAuth } from 'auth';
 
 const BASE_PATH = process.env.NEXT_PUBLIC_API_BASE ?? 'https://api.resources.occurnow.org/api/v1/';
 
+
 export async function makeRequest(method, path, params = {}, body, token) {
   const url = new URL(
     // Strip leading slash off of path
@@ -18,6 +19,7 @@ export async function makeRequest(method, path, params = {}, body, token) {
   return response.json();
 }
 
+
 export function getReq(path, params, token = null) {
   return makeRequest('get', path, params, undefined, token);
 }
@@ -30,28 +32,33 @@ export function putReq(path, params, body, token = null) {
   return makeRequest('put', path, params, body, token);
 }
 
-// TODO: add PATCH
+export function patchReq(path, params, body, token = null) {
+  return makeRequest('patch', path, params, body, token);
+}
 
 export function deleteReq(path, params, token = null) {
   return makeRequest('delete', path, params, undefined, token);
 }
 
+
 /* Hook to get a full suite of API functions that automatically pass auth token */
 export function useApi() {
   const { ready, isAuthenticated, accessToken } = useAuth();
   const tokenParam = (ready && isAuthenticated) ? accessToken : undefined;
-  const [getFunc, postFunc, putFunc, deleteFunc] = [getReq, postReq, putReq, deleteReq]
+  const [getf, postf, putf, patchf, deletef] = [getReq, postReq, putReq, patchReq, deleteReq]
     .map((func) => (...args) => func(...args, tokenParam));
   return {
     // API methods
-    get: getFunc,
-    post: postFunc,
-    put: putFunc,
-    delete: deleteFunc,
+    get: getf,
+    post: postf,
+    put: putf,
+    patch: patchf,
+    delete: deletef,
     // Info
     authenticated: ready && isAuthenticated,
   };
 }
+
 
 export default {
   useApi,
@@ -59,6 +66,7 @@ export default {
   get: getReq,
   post: postReq,
   put: putReq,
+  patch: patchReq,
   delete: deleteReq,
 
   request: makeRequest,
