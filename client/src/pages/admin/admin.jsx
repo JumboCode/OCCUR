52c470b9cbda9706520ab507e7eaa6343e563058
keisha mukasa <keisha.mukasa@gmail.com>
2021-04-19 17:48:59 -0400

    Added default temp admins

--- client/src/pages/admin/admin.jsx
+++ client/src/pages/admin/admin.jsx
@@ -3,16 +3,33 @@ import PropTypes from 'prop-types';
 
 import { isAdmin } from 'auth';
 import NotFound from 'pages/404';
+import styles from './admin.modules.scss';
+  
+// temporary list of admin users
+const ADMINUSERS = [
+    {
+      name: "Keisha Mukasa",
+      email: "keisha.mukasa@gmail.com"
+    },
+    {
+      name: "Luke Taylor",
+      email: "luketaylor@notrealemail.com"
+    }
+  ];
 
-const auth = useAuth();
-console.log(useAuth());  
-
-export default function ProtectedAdminRoute({ blocked }) {
+export default function AdminManager({ blocked }) {
     return blocked ? <NotFound /> : (
-      <div></div>
+        <div className="AdminDetails">
+        {ADMINUSERS.map((user) => (
+          <div className="AdminUser">
+            <input className="AdminName" type="text" value={user.name} />
+            <input className="AdminEmail" type="text" value={user.email} />
+          </div>
+        ))}
+      </div>
     );
   }
-  ProtectedAdminRoute.propTypes = {
+  AdminManager.propTypes = {
     blocked: PropTypes.bool.isRequired,
   };
   
