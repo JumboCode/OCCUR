120d08d3f38eaf371d85fd526a9a04ea01473df5
keisha mukasa <keisha.mukasa@gmail.com>
2021-04-22 17:04:40 -0400

    created add admin button

--- client/src/pages/admin/manager.jsx
+++ client/src/pages/admin/manager.jsx
@@ -2,42 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { isAdmin } from 'auth';
 import NotFound from 'pages/404';
-import styles from './manager.modules.scss';
+import styles from './manager.module.scss';
 
 import classNames from 'classnames/bind';
+import Circleplus from '../../../public/icons/circle_plus.svg';
+
 
 const cx = classNames.bind(styles);
-  
+
 // temporary list of admin users
 const ADMINUSERS = [
-    {
-      name: "Keisha Mukasa",
-      email: "keisha.mukasa@gmail.com"
-    },
-    {
-      name: "Luke Taylor",
-      email: "luketaylor@notrealemail.com"
-    }
-  ];
+  {
+    name: 'Keisha Mukasa',
+    email: 'keisha.mukasa@gmail.com',
+  },
+  {
+    name: 'Luke Taylor',
+    email: 'luketaylor@notrealemail.com',
+  },
+];
 
 export default function AdminManager({ blocked }) {
-    return blocked ? <NotFound /> : (
-        <div className={cx('base')}>
-        {ADMINUSERS.map((user) => (
-          <div className={cx('AdminUser')}>
-            <input className={cx('AdminName')} type="text" value={user.name} />
-            <input className={cx('AdminEmail')} type="text" value={user.email} />
-          </div>
-        ))}
+  return blocked ? <NotFound /> : (
+    <div className={cx('base')}>
+      <div className={cx('addAdmin')}>
+        <Circleplus className={cx('circleIcon')} />
+        <button className={cx('AddAdminButton')} type="button">Add Admin</button>
       </div>
-    );
-  }
-  AdminManager.propTypes = {
-    blocked: PropTypes.bool.isRequired,
-  };
-  
-  export function getServerSideProps(ctx) {
-    const blocked = !isAdmin(ctx);
-    if (blocked) ctx.res.statusCode = 404;
-    return { props: { blocked } };
-  }
+      {/* {ADMINUSERS.map((user) => (
+        <div className={cx('AdminUser')}>
+          <input className={cx('AdminName')} type="text" value={user.name} />
+          <input className={cx('AdminEmail')} type="text" value={user.email} />
+        </div>
+      ))} */}
+    </div>
+  );
+}
+
+AdminManager.propTypes = {
+  blocked: PropTypes.bool.isRequired,
+};
+
+export function getServerSideProps(ctx) {
+  const blocked = !isAdmin(ctx);
+  if (blocked) ctx.res.statusCode = 404;
+  return { props: { blocked } };
+}
