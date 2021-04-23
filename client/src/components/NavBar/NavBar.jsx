91cf00feb436c5f03b2d38517cfc164623a2944a
Luke Taylor <luke@deentaylor.com>
2021-04-23 11:30:08 -0400

    Conditional navbar links

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './NavBar.module.scss';
 import SearchBar from '../SearchBar/SearchBar.jsx';
 import Link from 'next/link';
+import { useAuth } from 'auth';
 
 
 export default function NavBar() {
+  const { isAuthenticated } = useAuth();
   return (
     <nav className={styles.base}>
       <Link href="/"><a className={styles.logo}><img alt="OCCUR logo" src="/logo.png" /></a></Link>
@@ -13,7 +15,21 @@ export default function NavBar() {
       <Link href="/"><a>Home</a></Link>
       <Link href="/resources"><a>Resources</a></Link>
       <a>Wifi Hotspot</a>
-      <a>Contact</a>
+      {
+        isAuthenticated
+          ? (
+            <>
+              {/* Links that only display for authenticated users */}
+              <a>Admin Manager</a>
+            </>
+          )
+          : (
+            <>
+              {/* Links that only display for unauthenticated users */}
+              <a>Contact</a>
+            </>
+          )
+      }
     </nav>
   );
 }
