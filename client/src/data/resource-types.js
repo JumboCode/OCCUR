fa480ffa8757bfd1ec84ed9f1d9ca3cf53dafb29
Luke Taylor <luke@deentaylor.com>
2021-04-23 12:22:46 -0400

    Move static data to central location

---
+++ client/src/data/resource-types.js
@@ -0,0 +1,10 @@
+export default [
+  { id: 'FOOD', label: 'Food' },
+  { id: 'HOUSING', label: 'Housing' },
+  { id: 'COMM_GIVE', label: 'Community Giveaways' },
+  { id: 'MENTAL_HEALTH', label: 'Mental Health' },
+  { id: 'INFO', label: 'Info Sessions/Webinars' },
+  { id: 'EVENTS', label: 'Events' },
+  { id: 'WIFI', label: 'Free Wifi' },
+  { id: 'OTHER', label: 'Other' },
+];
