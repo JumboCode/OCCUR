0c02714133fc9122c398ca6298ae7a21e27689f4
Luke Taylor <luke@deentaylor.com>
2021-04-24 15:54:45 -0400

    Update slugify

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
-export const slugify = (str) => str.toLowerCase().replace(/[^a-z0-9-'()]+/g, '-');
+export const slugify = (str, maxSegments = Infinity) => str
+  .toLowerCase()
+  .replace(/[^a-z0-9-'()]+/g, '-')
+  .split('-')
+  .slice(0, maxSegments)
+  .join('-');
