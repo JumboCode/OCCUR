3a979e58d1d5e611e7cd34e2a9268b9a7e56e980
keisha mukasa <keisha.mukasa@gmail.com>
2021-04-27 19:49:35 -0400

    adding changes to manager.jsx

--- client/src/pages/admin/manager.jsx
+++ client/src/pages/admin/manager.jsx
@@ -14,34 +14,21 @@ import Trash from '../../../public/icons/trash.svg';
 import Close from '../../../public/icons/close.svg';
 // import { render } from 'sass';
 
-
 const cx = classNames.bind(styles);
 // temporary list of admin users
-
-
-const ADMINUSERS = [
-  {
-    name: 'Keisha Mukasa',
-    email: 'keisha.mukasa@gmail.com',
-  },
-  {
-    name: 'Eddie Hatfield',
-    email: 'eddie.hatfield@whatever.com',
-  },
-  {
-    name: 'Tina King',
-    email: 'tina.king@whatever.com',
-  },
-  {
-    name: 'Luke Taylor',
-    email: 'luke.taylor@whatever.com',
-  },
-];
 
 export default function AdminManager({ blocked }) {
   const api = useApi();
   const [adminsLoaded, setAdminsLoaded] = useState(false);
   const [admins, setAdmins] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [modalType, setModalType] = useState('');
+  const { register, handleSubmit } = useForm();
+
+  function handleClick(type) {
+    setModal(!modal);
+    setModalType(type);
+  }
 
   useEffect(() => {
     if (api.authenticated) {
@@ -54,48 +41,71 @@ export default function AdminManager({ blocked }) {
     }
   }, [api.authenticated]);
 
-  const [modal, setModal] = useState(false);
-  function handleClick() {
-    setModal(!modal);
-  }
-  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  // const onSubmit = (data) => {
+  //   const newAdmin = {
+  //     name: data.adminName,
+  //     email: data.adminEmail,
+  //   };
+  //   if (api.authenticated) {
+  //     api.post('/new/admin', undefined, newAdmin).then((response) => {
+  //       console.log(response);
+  //       // setAdmins(response);
+  //       // setAdminsLoaded(true);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //     });
+  //     api.get('/list/admin').then((response) => {
+  //       setAdmins(response);
+  //       setAdminsLoaded(true);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //     });
+  //   }
+  // };
 
-  const onSubmit = (data) => {
-    const newAdmin = {
-      name: data.adminName,
-      email: data.adminEmail,
-    };
-    if (api.authenticated) {
-      api.post('/new/admin', undefined, newAdmin).then((response) => {
-        console.log(response);
-        // setAdmins(response);
-        // setAdminsLoaded(true);
-      }).catch((error) => {
-        console.log(error);
-      });
-      api.get('/list/admin').then((response) => {
-        setAdmins(response);
-        setAdminsLoaded(true);
-      }).catch((error) => {
-        console.log(error);
-      });
+  const Modal = () => {
+    let title = '';
+    if (modalType === 'add') {
+      title = <h4>Add Admin</h4>;
+    } else if (modalType === 'edit') {
+      title = <h4>Edit Admin</h4>;
     }
+    title = <h4>Delete Admin</h4>;
+
+    return (
+      <div className={cx('modalWindow')}>
+        <form>
+          <Close onClick={handleClick('')} className={cx('closeButton')} type="button" />
+          { title }
+          Admin Name
+          <input name="adminName" {...register('adminName')} placeholder="Admin Name" />
+          Admin Email
+          <input name="adminEmail" {...register('adminEmail')} placeholder="Admin Email" />
+          <button onClick={handleSubmit(onSubmit)} className={cx('saveButton')} type="button">Save</button>
+          <button onClick={handleClick('')} className={cx('cancelButton')} type="button">Cancel</button>
+        </form>
+      </div>
+    );
   };
 
-  const Modal = () => (
-    <div className={cx('modalWindow')}>
-      <form>
-        <Close onClick={handleClick} className={cx('closeButton')} type="button"/>
-        <h4>Add Admin</h4>
-        Admin Name
-        <input name="adminName" {...register('adminName')} placeholder="Admin Name" />
-        Admin Email
-        <input name="adminEmail" {...register('adminEmail')} placeholder="Admin Email" />
-        <button onClick={handleSubmit(onSubmit)} className={cx('saveButton')} type="button">Save</button>
-        <button onClick={handleClick} className={cx('cancelButton')} type="button">Cancel</button>
-      </form>
-    </div>
-  );
+  // const confirmDelete = (adminName) => {
+  //   console.log('in confirm delete');
+  //   return (
+  //     <div className={cx('modalWindow')}>
+  //       <form>
+  //         <Close onClick={handleClick('')} className={cx('closeButton')} type="button"/>
+  //         <h4>Delete Admin</h4>
+  //         Are you sure you would like to delete
+  //         {' '}
+  //         { adminName }
+  //         as an admin user?
+  //       </form>
+  //       <button onClick={handleClick('')} className={cx('saveButton')} type="button">Delete</button>
+  //       <button onClick={handleClick('')} className={cx('cancelButton')} type="button">Cancel</button>
+  //     </div>
+  //   );
+  // };
 
   const adminList = () => {
     if (!adminsLoaded) {
@@ -117,7 +127,7 @@ export default function AdminManager({ blocked }) {
       {modal ? <Modal /> : null }
       <div className={cx('mainAdminContainer')}>
         <div className={cx('buttonContainer')}>
-          <button className={cx('addAdmin')} type="button" onClick={handleClick}>
+          <button className={cx('addAdmin')} type="button" onClick={handleClick('add')}>
             <Circleplus className={cx('circleIcon')} />
             <div
               className={cx('addAdminButton')}
