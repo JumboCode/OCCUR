12381599d431548638acf92fcce4b11ffe2884dd
Luke Taylor <luke@deentaylor.com>
2021-05-03 13:44:23 -0400

    Live filtering from search bar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,40 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
-import styles from './SearchBar.module.scss';
+
 import SearchIcon from '../../../public/glass.svg';
+import styles from './SearchBar.module.scss';
 
 
-function SearchBar() {
+function SearchBar({ live }) {
   const [text, setText] = useState('');
   const router = useRouter();
 
+  // If we're already on the search page this function can just update the search shallowly instead
+  // of re-navigating
+  const updateSearch = useCallback((search) => {
+    if (router.pathname === '/resources') {
+      const { search: oldSearch, ...currentQuery } = router.query;
+      router.push({
+        pathname: '/resources',
+        query: { ...currentQuery, ...search && { search } },
+      }, undefined, { shallow: true });
+      return true;
+    }
+    return false;
+  }, [router]);
+
+  // In “live” mode, if we're already on the resources page, update the query as the user types.
+  useEffect(() => {
+    if (!live) return;
+    updateSearch(text);
+  }, [text, live]);
+
   const go = useCallback(() => {
-    router.push({ pathname: '/resources', query: text ? { search: text } : undefined });
+    const didShallowUpdate = updateSearch(text);
+    if (!didShallowUpdate) {
+      router.push({ pathname: '/resources', query: text ? { search: text } : undefined });
+    }
   }, [text]);
 
   return (
@@ -28,5 +53,7 @@ function SearchBar() {
     </div>
   );
 }
+SearchBar.propTypes = { live: PropTypes.bool };
+SearchBar.defaultProps = { live: false };
 
 export default SearchBar;
