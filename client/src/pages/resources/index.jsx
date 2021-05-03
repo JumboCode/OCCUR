72b59f2a90f82e8e9ada4ea73846fa52842a1789
Luke Taylor <luke@deentaylor.com>
2021-05-02 20:08:49 -0400

    Add bounds filtering to map

--- client/src/pages/resources/index.jsx
+++ client/src/pages/resources/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { useRouter } from 'next/router';
+import debounce from 'lodash.debounce';
+
 import ResourceCard from 'components/ResourceCard';
 import SidebarFilter from 'components/SidebarFilter/SidebarFilter';
 import Map from 'components/Map/Map';
@@ -12,6 +15,7 @@ import styles from './ResourceSearch.module.scss';
 
 export default function ResourcesPage({ data }) {
   const [values, setValues] = useState([]);
+  const router = useRouter();
 
   return (
     <div className={styles.base}>
@@ -21,7 +25,21 @@ export default function ResourcesPage({ data }) {
 
       <div className={styles.right}>
         <div className={styles.map}>
-          <Map resources={data} />
+          <Map
+            resources={data}
+            onMove={debounce(({ minLat, maxLat, minLng, maxLng }) => {
+              router.replace({
+                path: '/resources',
+                query: {
+                  ...router.query,
+                  min_lat: minLat.toFixed(5),
+                  max_lat: maxLat.toFixed(5),
+                  min_lng: minLng.toFixed(5),
+                  max_lng: maxLng.toFixed(5),
+                },
+              });
+            }, 500)}
+          />
         </div>
         { data.map((r) => (
           <ResourceCard key={r.id} {...r} />
@@ -35,9 +53,13 @@ ResourcesPage.propTypes = {
 };
 
 export async function getServerSideProps(context) {
-  const data = await api.get('/resources', context.query.search && {
-    search: context.query.search,
-  });
+  const data = await api.get(
+    '/resources',
+    ['search', 'min_lat', 'max_lat', 'min_lng', 'max_lng']
+      .filter((key) => context.query[key])
+      .map((key) => ({ [key]: context.query[key] }))
+      .reduce((accum, curr) => ({ ...accum, ...curr }), {}),
+  );
 
   return {
     props: { data },
